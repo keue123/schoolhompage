b0c17f0f5342c468e8df961e9b1137e962f86f6a
keue123 <95957333+keue123@users.noreply.github.com>
2022-06-19 23:25:14 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const content = "물리엔진";
+const text = document.querySelector(".text");
+var a = document.getElementById("hide");
+let i = 0;
+
+setTimeout(function() {
+    setInterval(typing, 150);
+}, 2000);
+
+function typing() {
+    if (i < content.length) {
+        let txt = content.charAt(i);
+        text.innerHTML += txt;
+        i++;
+    }
+}
